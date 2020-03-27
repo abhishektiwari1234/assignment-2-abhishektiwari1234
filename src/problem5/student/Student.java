3aad1fa84405be5c299cfa658f0a9d380cf36232
abhishektiwari <abhishek.tiwari_cs18@gla.ac.in>
2020-03-28 00:10:12 +0530

    declearing getter and setter methods

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -15,6 +15,32 @@ public class Student {
     public Student()();
 
     public Student(int roll, int backlog, int account) {
-        this.
+        this.roll = roll;
+        this.account = account;
+        this.backlog = backlog;
+    }
+
+    public int getAccount() {
+        return account;
+    }
+
+    public int getBacklog() {
+        return backlog;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
     }
 }
