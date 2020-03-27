d1578b757b1d4fec483c8726b6f126ea98f1e71c
abhishektiwari <abhishek.tiwari_cs18@gla.ac.in>
2020-03-28 00:00:04 +0530

    creating constructor

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -1,10 +1,20 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
+ *  User:vaibhav
  *  Date: 23-Mar-20
  *  Time: 7:15 PM
  */
 package problem3.myqueue;
 
+import problem3.node.Node;
+
 public class MyPriorityQueue {
+    public void insert
+    Node head;
+
+    public MyPriorityQueue() {
+        head = null;
+    }
+
+
 }
