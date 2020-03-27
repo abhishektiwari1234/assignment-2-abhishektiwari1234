c89cdc6a4e240ff3120ce76c268c6529fb15a48c
abhishektiwari <abhishek.tiwari_cs18@gla.ac.in>
2020-03-28 00:05:58 +0530

    creating instance variable

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -7,14 +7,21 @@
 package problem3.myqueue;
 
 import problem3.node.Node;
+import problem5.student.Student;
 
 public class MyPriorityQueue {
-    public void insert
     Node head;
 
     public MyPriorityQueue() {
         head = null;
     }
 
+    public void insert(String name, int roll) {
+        Node newnode = new Node();
+        Student obj = new Student();
+        obj.setName(name);
+        obj.setRoll(roll);
+        newNdoe.setobj(obj);
+    }
 
 }
--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -7,4 +7,14 @@
 package problem5.student;
 // to store student information and properties
 public class Student {
+    String Name;
+    int roll;
+    int account;
+    int backlog;
+
+    public Student()();
+
+    public Student(int roll, int backlog, int account) {
+        this.
+    }
 }
