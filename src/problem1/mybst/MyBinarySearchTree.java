de739116eaa9a69c121812dcf2707f1815478af0
abhishektiwari <abhishek.tiwari_cs18@gla.ac.in>
2020-03-27 23:26:24 +0530

    declaration to print left method

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -23,8 +23,41 @@ public class MyBinarySearchTree {
         if (root == null) {
             root = newnode;
         } else {
-
+            TreeNode current = root;
+            TreeNode parent;
+            while (true) {
+                parent = current;
+                if (val < current.getData()) {
+                    current = current.getLeft();
+                }
+                if (current == null) {
+                    parent.setLeft(newnode);
+                    return;
+                } else {
+                    current = current.getRight();
+                    if (current == null) {
+                        parent.setRight(newnode);
+                        return;
+                    }
+                }
+            }
         }
     }
 
+    public void printLeftNode(TreeNode root) {
+        if (root == null) {
+            return;
+        } else {
+            if (root.getLeft() != null) {
+                System.out.print(root.getLeft().getData() + " ");
+            } else {
+                c++;
+            }
+            printLeftNode(root.getLeft());
+            printLeftNode(root.getRight());
+        }
+
+    }
+
+    public
 }
