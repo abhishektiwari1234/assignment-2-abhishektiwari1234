56f88c66d7e99ad5f75e5ac1ec1d9330fff1b893
abhishektiwari <abhishek.tiwari_cs18@gla.ac.in>
2020-03-27 23:50:22 +0530

    inserting the methods and inserting the values

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -7,9 +7,23 @@
 package problem2.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
+
+import problem1.mybst.MyBinarySearchTree;
 public class MyMain {
     public static void main(String[] args) {
-
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(5);
+        obj.insert(2);
+        obj.insert(7);
+        obj.insert(6);
+        obj.insert(4);
+        obj.insert(9);
+        obj.insert(11);
+        obj.preOrder(obj.root);
+        System.out.println();
+        obj.postOrder(obj.root);
+        System.out.println("\n First statement is true");
+        System.out.println("Second is false as we did not get the middle point");
     }
 }
 
