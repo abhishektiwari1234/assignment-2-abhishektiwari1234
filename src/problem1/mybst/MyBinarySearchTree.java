8df209bf239726872a83a120bcd1b36b0bab4bb1
abhishektiwari <abhishek.tiwari_cs18@gla.ac.in>
2020-03-27 23:09:50 +0530

    insertion method signature

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,6 +5,26 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+
+import problem1.node.TreeNode;
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+    public TreeNode root;
+    int c;
+
+    public void myBinarySearchTree() {
+        root = null;
+    }
+
+    public void insert(int val) {
+        TreeNode newnode = new TreeNode();
+        newnode.setData(val);
+        if (root == null) {
+            root = newnode;
+        } else {
+
+        }
+    }
+
 }
--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -19,6 +19,21 @@ public class TreeNode {
         this.data = data;
     }
 
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
 
 
 }
