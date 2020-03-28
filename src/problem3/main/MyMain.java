c9f5c73b3056379dbe09e54666b87afe01928e47
abhishektiwari <abhishek.tiwari_cs18@gla.ac.in>
2020-03-28 13:47:56 +0530

    creating main method in which inserting values and display them using peep() method

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -6,7 +6,18 @@
  */
 package problem3.main;
 
+import problem3.myqueue.MyPriorityQueue;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue obj = new MyPriorityQueue();
+        obj.insert("Abhi", 20);
+        obj.insert("Mohan", 25);
+        obj.insert("Muskan", 30);
+        obj.insert("Prashant", 28);
+        System.out.println("Students according to their Rollno are: ");
+        obj.peep();
+    }
 }
