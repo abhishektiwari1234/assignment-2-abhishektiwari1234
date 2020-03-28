f3b486d13a68bb56db0a05f7e894e5ee59eafc92
abhishektiwari <abhishek.tiwari_cs18@gla.ac.in>
2020-03-28 14:32:47 +0530

    creating main method and inserting values and display them

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -8,5 +8,22 @@ package problem4.main;
 
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
+
+import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(10);
+        obj.insert(12);
+        obj.insert(8);
+        obj.insert(9);
+        obj.insert(17);
+        obj.insert(7);
+        obj.insert(19);
+        MyQueue obj1 = new MyQueue();
+        obj1.preOrderSucc(obj.root);
+        System.out.print("Pre Order successor of all the nodes are: ");
+        obj1.disPreOrderSucc();
+    }
 }
--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -7,6 +7,42 @@
 package problem4.myqueue;
 // to create queue to store pre - order successor
 
+import problem1.node.TreeNode;
+import problem4.node.Node;
 public class MyQueue {
+    Node front;
+    Node rear;
 
+    public MyQueue() {
+        this.front = null;
+        this.rear = null;
+    }
+
+    public void enqueue(int data) {
+        Node newNode = new Ndoe();
+        newNode.setData(data);
+        if (front == null) {
+            front = newNode;
+            rear = newNode;
+        } else {
+            rear.setNext(newNdoe);
+            rear = newNdoe;
+        }
+    }
+
+    public void preOrderSucc(TreeNode root) {
+        if (root != null) {
+            enqueue(root.getData());
+            preOrderSucc(root.getLeft());
+            preOrderSucc(root.getRight());
+        }
+    }
+
+    public void disPreOrderSucc() {
+        Node temp = front.getNext();\
+        while (temp != null) {
+            System.out.println(temp.getData() + " ");
+            temp = temp.getNext();
+        }
+    }
 }
