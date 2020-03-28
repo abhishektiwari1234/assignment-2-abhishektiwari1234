f0e1b9adce874b7439b4fc1a933d8d69b9124489
abhishektiwari <abhishek.tiwari_cs18@gla.ac.in>
2020-03-28 13:39:33 +0530

    creating two methods

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -17,11 +17,31 @@ public class MyPriorityQueue {
     }
 
     public void insert(String name, int roll) {
-        Node newnode = new Node();
+        Node newNode = new Node();
         Student obj = new Student();
         obj.setName(name);
         obj.setRoll(roll);
-        newnode.setobj(obj);
+        newNode.setObj(obj);
+        Node temp = head;
+        if (head == null) {
+            head = newNode;
+        } else {
+            if (head.getObj().getRoll() > roll) {
+                temp = temp.getNext();
+            } else {
+                while (temp.getNext() != null && temp.getNext().getObj().getRoll() < roll) {
+                    temp = temp.getNext();
+                }
+                newNode.setNext(temp.getNext());
+                temp.setNext(newNode);
+            }
+        }
+        public void peep ()
+        {
+            Node temp = head;
+            while (temp != null) {
+                System.out.println(temp.getObj().getName() + " "temp.getObj().getName());
+            }
+        }
     }
-
 }
